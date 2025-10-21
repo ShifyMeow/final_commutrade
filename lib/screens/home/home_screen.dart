f399e520dbdebc79109e26163e845e100dc79d1a
ShifyMeow <masterashify@gmail.com>
2025-10-21 20:50:39 +0800

    Version 0.0.2

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,9 +1,39 @@
 import 'package:final_commutrade/theme/app_theme_notifier.dart';
+import 'package:final_commutrade/widgets/item_card.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
+
+  // Placeholder data - in a real app, this would come from a database.
+  final List<Map<String, String>> _dummyItems = const [
+    {
+      'title': 'Vintage Leather Jacket',
+      'price': 'Trade for Watch',
+      'author': 'user123',
+    },
+    {
+      'title': 'Classic Acoustic Guitar',
+      'price': 'Trade/Sell: \$150',
+      'author': 'musician_max',
+    },
+    {
+      'title': 'Set of 4 Nordic Chairs',
+      'price': 'Trade for Bookshelf',
+      'author': 'design_dreamer',
+    },
+    {
+      'title': 'Rare Comic Book Collection',
+      'price': 'Trade Only',
+      'author': 'collector_carl',
+    },
+    {
+      'title': 'Professional Camera Lens',
+      'price': 'Trade/Sell: \$200',
+      'author': 'photo_pro',
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -28,36 +58,19 @@ class HomeScreen extends StatelessWidget {
             },
           ),
         ],
-        // Adding a subtle shadow to the AppBar
         elevation: 4.0,
         backgroundColor: Theme.of(context).cardColor,
       ),
-      body: const Center(
-        child: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(
-                Icons.home_work_outlined,
-                size: 80,
-                color: Colors.grey,
-              ),
-              SizedBox(height: 20),
-              Text(
-                'Welcome to CommuTrade!',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: 10),
-              Text(
-                'This is the home feed. Featured items and updates will appear here.',
-                style: TextStyle(fontSize: 16, color: Colors.grey),
-                textAlign: TextAlign.center,
-              ),
-            ],
-          ),
-        ),
+      body: ListView.builder(
+        itemCount: _dummyItems.length,
+        itemBuilder: (BuildContext context, int index) {
+          final item = _dummyItems[index];
+          return ItemCard(
+            title: item['title']!,
+            price: item['price']!,
+            author: item['author']!,
+          );
+        },
       ),
     );
   }
